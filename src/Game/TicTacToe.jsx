44bb1fc547fabc1aa041f1ba5af465d8ddb22ba7
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-07 16:22:04 +0530

    box styling completed

--- src/Game/TicTacToe.jsx
+++ src/Game/TicTacToe.jsx
@@ -8,7 +8,21 @@ export default function Game() {
     <div className="container">
       <h1 className="title">Tic-Tac-Toe</h1>
       <div className="board">
-            
+            <div className="row1">
+              <div className="boxes"></div>
+              <div className="boxes"></div>
+              <div className="boxes"></div>
+            </div>
+            <div className="row2">
+              <div className="boxes"></div>
+              <div className="boxes"></div>
+              <div className="boxes"></div>
+            </div>
+            <div className="row3">
+              <div className="boxes"></div>
+              <div className="boxes"></div>
+              <div className="boxes"></div>
+            </div>
       </div>
       <button className="reset">Reset</button>
     </div>
